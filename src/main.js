04d878dc30997b2bd8c7d0d877a6ff7d40e0b4be
fedorivrv <fedoriv.roman.v@gmail.com>
2025-05-19 14:15:31 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-import {menuIcon,useElement, mobileMenu} from "./burger.js";
+document.addEventListener('DOMContentLoaded', () => {
+  const menuIcon = document.getElementById('menu-icon');
+  const useElement = menuIcon.querySelector('use');
+  const mobileMenu = document.getElementById('mobileMenu');
+  menuIcon.addEventListener('click', () => {
+    mobileMenu.classList.toggle('open');
+    const currentIcon = useElement.getAttribute('href');
+    const isMenuOpen = currentIcon.includes('icon-menu');
+    useElement.setAttribute(
+      'href',
+      isMenuOpen ? '/img/icons.svg#icon-close' : '/img/icons.svg#icon-menu'
+    );
+  });
+});
