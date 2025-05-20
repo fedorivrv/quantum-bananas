c868a01b724be6dd73e5ee231e51a0f11dc9e09b
Nadiia Rakivska <rakivska94@gmail.com>
2025-05-20 15:07:53 +0300

    changed burger js file

--- src/burger.js
+++ src/burger.js
@@ -5,9 +5,25 @@
     body: document.body,
   };
 
+  // Функція для закриття меню
+  const closeMenu = () => {
+    refs.menu.classList.remove('is-open');
+    refs.toggleMenuBtn.classList.remove('is-open');
+    refs.body.classList.remove('no-scroll');
+  };
+
+  // Перемикання меню по кнопці
   refs.toggleMenuBtn.addEventListener('click', () => {
     const isOpen = refs.menu.classList.toggle('is-open');
     refs.toggleMenuBtn.classList.toggle('is-open');
     refs.body.classList.toggle('no-scroll', isOpen);
+  });
+
+  // Закриття меню при кліку на будь-яке посилання в бургер-меню
+  const menuLinks = refs.menu.querySelectorAll('a');
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      closeMenu();
+    });
   });
 })();
