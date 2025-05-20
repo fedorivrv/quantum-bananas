be25f55d3df23176ec8b2fc321cf311eeca2a98d
Nadiia Rakivska <rakivska94@gmail.com>
2025-05-20 13:18:08 +0300

    changed

--- src/burger.js
+++ src/burger.js
@@ -2,10 +2,12 @@
   const refs = {
     toggleMenuBtn: document.querySelector('[data-menu-toggle]'),
     menu: document.querySelector('[data-menu]'),
+    body: document.body,
   };
 
   refs.toggleMenuBtn.addEventListener('click', () => {
+    const isOpen = refs.menu.classList.toggle('is-open');
     refs.toggleMenuBtn.classList.toggle('is-open');
-    refs.menu.classList.toggle('is-open');
+    refs.body.classList.toggle('no-scroll', isOpen);
   });
 })();
